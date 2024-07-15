a33d96a8c8c5ace78671a82a164a41137348938c
jeremy avnet <brainsik@users.noreply.github.com>
2024-07-15 10:25:26 -0700

    Refactor

--- whats-blocking.py
+++ whats-blocking.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python3
 
 import argparse
-import enum
 import json
 import os
 import time
+from collections import defaultdict
+from typing import Any, Callable, TypedDict
 from urllib.request import urlopen, Request
 
 URL_BASE = "https://api.nextdns.io/profiles/{}"
 URL_TMPL = URL_BASE + "/logs?status=blocked&limit=1000"
 
 
-def get_test_data(profile_id: str = ""):
-    return [
-        dict(reasons=[dict(id="a")], domain="a"),
-        dict(reasons=[dict(id="b")], domain="b"),
-        dict(reasons=[dict(id="c"), dict(id="d")], domain="cd"),
-    ]
+NextDnsJson = Any
+DomData = dict[str, set[str]]  # d[domain] = set[blists]
+BlistData = dict[str, set[str]]  # d[blist_id] = set[domains]
 
 
-def get_api_data(api_key: str, profile_id: str, save: bool = False):
+class ConfigJson(TypedDict):
+    api_key: str
+    profiles: dict[str, str]
+
+
+def json_to_domdata(jsondata: NextDnsJson) -> DomData:
+    output: DomData = {}
+
+    for entry in jsondata:
+        blocklists = {r["id"] for r in entry["reasons"]}
+        output[entry["domain"]] = blocklists
+
+    return output
+
+
+def domdata_to_blistdata(domdata: DomData) -> BlistData:
+    blistdata: BlistData = defaultdict(set)
+
+    for dom in domdata:
+        for blist in domdata[dom]:
+            blistdata[blist].add(dom)
+
+    return blistdata
+
+
+def get_api_data(api_key: str, profile_id: str, keep: bool = False) -> NextDnsJson:
     url = URL_TMPL.format(profile_id)
 
     # Requests are blocked (403) when the default urllib user-agent is sent (WTF?)
@@ -27,103 +50,76 @@ def get_api_data(api_key: str, profile_id: str, save: bool = False):
     resp = urlopen(
         Request(url, headers={"X-Api-Key": api_key, "User-Agent": "Phi/1.618"})
     )
+    data: NextDnsJson = json.loads(resp.read())
 
-    data = json.loads(resp.read())["data"]
-
-    if save:
-        fname = "{}-{}.json".format(profile_id, time.time())
+    if keep:
+        fname = "{}-{}.log.json".format(profile_id, time.time())
         with open(fname, "w") as fo:
             json.dump(data, fo)
-            print("💾 Wrote {}".format(fname))
-    return data
+            print("💾 Wrote", fname)
+
+    print("✅ Found {} log entries".format(len(data["data"])))
+    return json_to_domdata(data["data"])
 
 
-def get_file_data(filename: str):
-    with open(filename, "r") as fi:
-        print("💾 Loading JSON from {}".format(filename))
-        return json.load(fi)
+def get_file_data(fname: str) -> NextDnsJson:
+    with open(fname, "r") as fi:
+        print("💾 Loading JSON from", fname)
+        data: NextDnsJson = json.load(fi)
+
+    print("✅ Found {} log entries".format(len(data["data"])))
+    return json_to_domdata(data["data"])
 
 
-def main(args: argparse.Namespace, config: dict[str, str]):
-    solos: dict[str, set[str]] = {}
-    combos: dict[tuple[str, ...], set[str]] = {}
+def process_domdata(domdata: DomData):
+    print("⚙️  Processing {} domains".format(len(domdata)))
 
-    domains: set[str] = set()
-    domain_hits: dict[str, set[str]] = {}  # [blocklist, {domains}]
+    solos: dict[str, set[str]] = defaultdict(set)  # blist -> doms
+    combos: dict[tuple[str, ...], set[str]] = defaultdict(set)  # blists -> doms
+    overlap: dict[str, list[int]] = defaultdict(list)  # blist -> redundancy
 
-    redundancy: dict[str, list[int]] = {}  # [blocklist, [redundancy num, …]]
+    for dom in domdata:
+        for blist in domdata[dom]:
+            overlap[blist].append(len(domdata[dom]))
 
-    if args.profile:
-        api_key = os.environ.get("NEXTDNS_API_KEY", config["api_key"])
-        profile_id = config["profiles"][args.profile]
-        data = get_api_data(api_key, profile_id, args.save)
-    elif args.file:
-        data = get_file_data(args.file)
-    else:
-        print("No profile or file specified!")  # shouldn't get here
-        raise SystemExit(1)
+        if len(domdata[dom]) == 1:
+            blist = list(domdata[dom])[0]
+            solos[blist].add(dom)
 
-    print("✅ Found {} entries".format(len(data)))
-
-    for entry in data:
-        domain = entry["domain"]
-
-        if domain in domains:
+    # 2nd pass: find lists only appearing with other lists, but not the solos
+    for dom in domdata:
+        if solos.keys() & domdata[dom]:
             continue
-        domains.add(domain)
 
-        for r in entry["reasons"]:
-            r_id = r["id"]
-            domain_hits.setdefault(r_id, set())
-            domain_hits[r_id].add(domain)
-
-            redundancy.setdefault(r_id, [])
-            redundancy[r_id].append(len(entry["reasons"]))
-
-            # separately track blocklists appearing on their own
-            if len(entry["reasons"]) == 1:
-                solos.setdefault(r_id, set())
-                solos[r_id].add(domain)
-                continue
-
-    # 2nd pass: find entries only appearing with other entries that aren't unique
-    for entry in data:
-        r_ids = set([r["id"] for r in entry["reasons"]])
-        if solos.keys() & r_ids:  # keep going if any of these blocklists are in solos
-            continue
-        k = tuple(sorted(r_ids))
-        combos.setdefault(k, set())
-        combos[k].add(entry["domain"])
+        blists = tuple(sorted(domdata[dom]))
+        combos[blists].add(dom)
 
     print("\n#\n# Blocklists appearing by themselves\n#\n")
     print("domains\tid")
     print("--     \t--")
-    for bl_id in sorted(solos):
-        print("{}\t{}\n\t{}".format(len(solos[bl_id]), bl_id, solos[bl_id]))
+    for blist in sorted(solos):
+        print("{}\t{}\n\t{}".format(len(solos[blist]), blist, solos[blist]))
 
     if combos:
         print("\n#\n# Blocklists found only in combos\n#\n")
         print("domains\tid")
         print("--     \t--")
-        for bl_ids in combos:
-            print("{}\t{}\n\t{}".format(len(combos[bl_ids]), bl_ids, combos[bl_ids]))
+        for blists in combos:
+            print("{}\t{}\n\t{}".format(len(combos[blists]), blists, combos[blists]))
 
-    print("\n#\n# Domain coverage ({} total)\n#\n".format(len(domains)))
-    for r_id in sorted(
-        domain_hits,
-        key=lambda k: len(domain_hits[k]) / len(domains),
-        reverse=True,
-    ):
-        pct = len(domain_hits[r_id]) / len(domains)
-        print("{:4.1f}% {}".format(100 * pct, r_id))
+    blistdata = domdata_to_blistdata(domdata)
+    coverage_pct: Callable[[str], float]
+    coverage_pct = lambda blist: len(blistdata[blist]) / len(domdata)
+    print("\n#\n# Domain coverage ({} total)\n#\n".format(len(domdata)))
+    for blist in sorted(blistdata, key=coverage_pct, reverse=True):
+        print("{:4.1f}% {}".format(100 * coverage_pct(blist), blist))
 
     print("\n#\n# Redundancy histogram\n#\n")
-    for r_id in sorted(redundancy):
-        level_hist: dict[int, int] = {}
-        for level in redundancy[r_id]:
-            level_hist.setdefault(level, 0)
+    for blist in sorted(overlap):
+        level_hist: dict[int, int] = defaultdict(int)
+        for level in overlap[blist]:
             level_hist[level] += 1
-        print(r_id)
+        print(blist)
 
         for n in range(1, max(level_hist.keys()) + 1):
             level_str = "{:2d}".format(n)
@@ -138,16 +134,11 @@ def main(args: argparse.Namespace, config: dict[str, str]):
         print()
 
 
-def get_config(fname: str):
-    with open(fname, "r") as fi:
-        return json.load(fi)
-
-
 def get_args():
     parser = argparse.ArgumentParser(description="Find blocklists in use.")
     parser.add_argument("-c", "--config", default="config.json")
     parser.add_argument(
-        "-s", "--save", help="Save downloaded data", action="store_true"
+        "-k", "--keep", help="Keep downloaded data in a file", action="store_true"
     )
 
     group = parser.add_mutually_exclusive_group(required=True)
@@ -157,6 +148,27 @@ def get_args():
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def get_config(fname: str) -> ConfigJson:
+    with open(fname, "r") as fi:
+        return json.load(fi)
+
+
+def main():
     args = get_args()
-    main(args, get_config(args.config))
+    config = get_config(args.config)
+
+    if args.profile:
+        api_key = os.environ.get("NEXTDNS_API_KEY", config["api_key"])
+        profile_id = config["profiles"][args.profile]
+        domdata = get_api_data(api_key, profile_id, args.keep)
+    elif args.file:
+        domdata = get_file_data(args.file)
+    else:
+        print("No profile or file specified!")  # shouldn't get here
+        raise SystemExit(1)
+
+    process_domdata(domdata)
+
+
+if __name__ == "__main__":
+    main()
